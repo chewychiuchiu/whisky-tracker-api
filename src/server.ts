35fbea3b32d2ff01dc49b130d98ba04680696f87
Alex Chiu <alexanderchiu3@gmail.com>
2026-06-18 17:21:23 -0500

    Fix server binding for Render

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
 import app from './app'
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Running at http://127.0.0.1:${PORT}`)
+app.listen(Number(PORT), '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`)
+  console.log(`Swagger docs at  http://0.0.0.0:${PORT}/api-docs`)
 })
